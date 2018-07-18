7fce90624d8965ec9ae9fd2ab98b92e941812665
javam2018 <javam2018@S10-96645.redes.local>
2018-07-18 09:56:48 -0600

    Agregé la clase conexion

---
+++ CentroMedico/src/java/com/centromedico1/modelo/Conexion.java
@@ -0,0 +1,42 @@
+
+package com.centromedico1.modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Conexion {
+    
+    private Connection conexion;
+    private static final String URL = "jdbc:mysql://localhost:3306/¿clinica_medica";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234";
+    
+    public Connection getMiconexion(){
+        return conexion;
+    }
+    public void setMiconexion(Connection conexion){
+        this.conexion = conexion;
+    }
+    public void Conector() throws SQLException {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException ex){
+            System.out.println("Error en conexion" + ex);
+        }
+    }
+    public void cerrarConexion(){
+        try{
+            if (conexion != null) {
+                if (conexion.isClosed()==false){
+                    conexion.close();
+                }
+            }
+        } catch (SQLException ex){
+            System.out.println("Error al cerrar" + ex);
+        }
+    }
+    
+}
