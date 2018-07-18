7fb6bc50b967a17e6d1569c55208afd50a3e53dd
Usuario <Usuario@10.0.2.15>
2018-07-18 09:58:18 -0600

    Le cambie la primer letra de clase persona a mayuscula (Persona)

--- CentroMedico/src/java/com/centromedico1/modelo/Persona.java
+++ CentroMedico/src/java/com/centromedico1/modelo/Persona.java
@@ -5,117 +5,54 @@
  */
 package com.centromedico1.modelo;
 
-import java.sql.Date;
-
 /**
  *
  * @author Usuario
  */
 public class Persona {
-    private Integer Cui;
-    private Integer Telefono;
-    private String pNombre;
-    private String pApellido;
-    private String sNombre;
-    private String sApellido;
-    private String Genero;
-    private String Direccion;
-    private String Email;
-    private Date FechaNacimiento;
-
-    public Persona() {
-        this.Cui = null;
-        this.Telefono = null;
-        this.pNombre = null;
-        this.pApellido = null;
-        this.sNombre = null;
-        this.sApellido = null;
-        this.Genero = null;
-        this.Direccion = null;
-        this.Email = null;
-        this.FechaNacimiento = null;
-    }
-
     
-    public Integer getCui() {
-        return Cui;
+    private String nombre;
+    private String apellido;
+    private Integer telefono;
+    private String direccion;
+
+    public Persona(){
+        this.nombre=null;
+        this.apellido=null;
+        this.direccion=null;
+        this.telefono=null;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String getApellido() {
+        return apellido;
     }
 
-    public void setCui(Integer Cui) {
-        this.Cui = Cui;
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
     }
 
     public Integer getTelefono() {
-        return Telefono;
+        return telefono;
     }
 
-    public void setTelefono(Integer Telefono) {
-        this.Telefono = Telefono;
-    }
-
-    public String getpNombre() {
-        return pNombre;
-    }
-
-    public void setpNombre(String pNombre) {
-        this.pNombre = pNombre;
-    }
-
-    public String getpApellido() {
-        return pApellido;
-    }
-
-    public void setpApellido(String pApellido) {
-        this.pApellido = pApellido;
-    }
-
-    public String getsNombre() {
-        return sNombre;
-    }
-
-    public void setsNombre(String sNombre) {
-        this.sNombre = sNombre;
-    }
-
-    public String getsApellido() {
-        return sApellido;
-    }
-
-    public void setsApellido(String sApellido) {
-        this.sApellido = sApellido;
-    }
-
-    public String getGenero() {
-        return Genero;
-    }
-
-    public void setGenero(String Genero) {
-        this.Genero = Genero;
+    public void setTelefono(Integer telefono) {
+        this.telefono = telefono;
     }
 
     public String getDireccion() {
-        return Direccion;
+        return direccion;
     }
 
-    public void setDireccion(String Direccion) {
-        this.Direccion = Direccion;
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
     }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String Email) {
-        this.Email = Email;
-    }
-
-    public Date getFechaNacimiento() {
-        return FechaNacimiento;
-    }
-
-    public void setFechaNacimiento(Date FechaNacimiento) {
-        this.FechaNacimiento = FechaNacimiento;
-    }
-    
     
 }
