e7b767bbf258666ce175d900b399bfd86fe7f66f
Usuario <Usuario@10.0.2.15>
2018-07-18 08:53:42 -0600

    Le agregue a la clase persona campos de tabla + getter y setter + constructor

--- CentroMedico/src/java/com/centromedico1/modelo/persona.java
+++ CentroMedico/src/java/com/centromedico1/modelo/persona.java
@@ -5,10 +5,117 @@
  */
 package com.centromedico1.modelo;
 
+import java.sql.Date;
+
 /**
  *
  * @author Usuario
  */
 public class persona {
+    private Integer Cui;
+    private Integer Telefono;
+    private String pNombre;
+    private String pApellido;
+    private String sNombre;
+    private String sApellido;
+    private String Genero;
+    private String Direccion;
+    private String Email;
+    private Date FechaNacimiento;
+
+    public persona() {
+        this.Cui = null;
+        this.Telefono = null;
+        this.pNombre = null;
+        this.pApellido = null;
+        this.sNombre = null;
+        this.sApellido = null;
+        this.Genero = null;
+        this.Direccion = null;
+        this.Email = null;
+        this.FechaNacimiento = null;
+    }
+
+    
+    public Integer getCui() {
+        return Cui;
+    }
+
+    public void setCui(Integer Cui) {
+        this.Cui = Cui;
+    }
+
+    public Integer getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(Integer Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getpNombre() {
+        return pNombre;
+    }
+
+    public void setpNombre(String pNombre) {
+        this.pNombre = pNombre;
+    }
+
+    public String getpApellido() {
+        return pApellido;
+    }
+
+    public void setpApellido(String pApellido) {
+        this.pApellido = pApellido;
+    }
+
+    public String getsNombre() {
+        return sNombre;
+    }
+
+    public void setsNombre(String sNombre) {
+        this.sNombre = sNombre;
+    }
+
+    public String getsApellido() {
+        return sApellido;
+    }
+
+    public void setsApellido(String sApellido) {
+        this.sApellido = sApellido;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public Date getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date FechaNacimiento) {
+        this.FechaNacimiento = FechaNacimiento;
+    }
+    
     
 }
