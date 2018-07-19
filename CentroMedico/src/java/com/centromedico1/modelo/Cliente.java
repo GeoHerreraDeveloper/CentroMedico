4286cab42109686275e98c9c9a7ae080e636cc72
Usuario <Usuario@10.0.2.15>
2018-07-19 10:53:27 -0600

    Creacion de clase Clientes. GeoHerrera

---
+++ CentroMedico/src/java/com/centromedico1/modelo/Cliente.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.centromedico1.modelo;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cliente extends Personas{
+    private Integer Cod_cliente;
+
+    public Cliente() {
+        this.Cod_cliente=0;
+    }
+
+    public Cliente(Integer Cod_cliente, Integer Cui, String pNombre, String pApellido, String sNombre, String sApellido, String Genero, Date FechaNacimiento, String Direccion, Integer Telefono, String Email) {
+        super(Cui, pNombre, pApellido, sNombre, sApellido, Genero, FechaNacimiento, Direccion, Telefono, Email);
+        this.Cod_cliente = Cod_cliente;
+    }
+
+    public Integer getCod_cliente() {
+        return Cod_cliente;
+    }
+
+    public void setCod_cliente(Integer Cod_cliente) {
+        this.Cod_cliente = Cod_cliente;
+    }
+    
+    
+    
+    
+}
